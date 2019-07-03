d590f622f3c17332b3a9eb484208be1d4508961f
laerling <laerling@posteo.de>
2019-07-03 19:09:02 +0200

    Some unit tests for the VGA buffer

--- src/main.rs
+++ src/main.rs
@@ -54,7 +54,6 @@ fn trivial_assertion() {
     serial_print!("trivial assertion... ");
     assert_eq!(1, 1);
     serial_println!("[ok]");
-    loop {}
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
