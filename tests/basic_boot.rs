3a50d21e2752f98d5c034f1a4ab3041cde5d9c49
laerling <laerling@posteo.de>
2019-07-04 01:51:08 +0200

    Add test to basic_boot.rs

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -6,6 +6,8 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
+use blog_os::{println, serial_print, serial_println};
+
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
@@ -17,4 +19,11 @@ pub extern "C" fn _start() -> ! {
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blog_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn test_println() {
+    serial_print!("test_println... ");
+    println!("test_println output");
+    serial_println!("[ok]");
 }
