f5abd22f530f7f7c38c5a9eebccb048084d2e5c2
laerling <laerling@posteo.de>
2019-07-05 17:44:59 +0200

    Register exception handler for breakpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,17 @@
 #![no_std]
+
+// testing
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+// exceptions and interrupts
+#![feature(abi_x86_interrupt)]
+
 pub mod serial;
 pub mod vga_buffer;
+pub mod interrupts;
 
 use core::panic::PanicInfo;
 
